71208e819da28650e967f94c50b73000a42318ac
Dylan Michael Wheatley <dylan.m.wheatley@gmail.com>
2015-12-28 18:04:01 -0800

    retab

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 void fizzbuzz() {
-	int i;	// pre-c99 style declaration outside for loop
-	for (i = 1; i <= 100; ++i) {
-		if (i % 3 == 0)
-			if (i % 5 == 0)
-				printf("%d\tfizzbuzz\n", i);
-			else printf("%d\tfizz\n", i);
-		else if (i % 5 == 0)
-			printf("%d\tbuzz\n", i);
-		else printf("%d\n", i);
-	}
+  int i;  // pre-c99 style declaration outside for loop
+  for (i = 1; i <= 100; ++i) {
+    if (i % 3 == 0)
+      if (i % 5 == 0)
+        printf("%d\tfizzbuzz\n", i);
+      else printf("%d\tfizz\n", i);
+    else if (i % 5 == 0)
+      printf("%d\tbuzz\n", i);
+    else printf("%d\n", i);
+  }
 }
 
 int main() {
-	fizzbuzz();
-	return 0;
+  fizzbuzz();
+  return 0;
 }
